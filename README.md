2cc19fe2f925169dd5c32db05e0478ef4f7e1c81
Bl4ckeT <73257561+Bl4ckeT@users.noreply.github.com>
2020-10-23 23:26:59 -0300

    sub1
    src + hora.c

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SSOO-tarea01
+
+{Alonso Benjamín Faúndez Garcia, Alonso.faundez@alumnos.uv.cl}
+
+El codigo cuenta con 3 funciones signalCtrl, signalHandler y start, 
+donde signalCtrl se encarga de ignorar la una señal de "ctrl+c" para
+que este cierre realizando la accion 2 veces, por otro lado signalHandler
+se acciona al captar la señal "SIGUSR1", si esa señal es captada manda
+datos de fecha y hora, y start nos puetra le pid del proceso inicial y deja 
+activo el capto de la señal SIGUSR1, finalmente en el Main se deja activo el capto
+de la señal que activa el sginalCtrl para que ignore la primera señal al apretar "ctrl+c" 
+tambien se hace un while para dejar el proceso activo y se usa un pause para que este espere
+la siguente señal. 
---
+++ src/hora.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include <signal.h>
+#include <unistd.h>
+
+void signalCtrl(int senhal){  //Esta funcion se encarga de ignorar la señal
+			      //logrnado que se cierre el programa 
+        signal(SIGINT, SIG_DFL); //aprentando 2 veces ctrl+c 
+
+}
+
+void signalHandler(int senhal){ 
+//Esta funcion dice cuando capta la señal SIGUSR1 y retorna los datos 
+//DIA/MES/NUM_DIA/HORA/AÑO
+
+   	time_t tiempo = time(0);
+        struct tm *tlocal = localtime(&tiempo);
+       	char output[128];
+	strftime(output,128,"%a %b %d %H:%M:%S %Y",tlocal);
+
+	if (senhal == SIGUSR1) {
+		printf("hi");
+		printf("Señal SIGUSR1 recibida: %s", output);
+		printf("\nListo para recibir la señal SIGUSR1.\n");
+		pause();
+
+   }
+
+	else{printf("No hay señal");}
+
+}
+
+
+void start(){ // Esta funcion pide el pid del proceso acual 
+
+		printf("Programa hora ejecutandose. PID=%d.\n", getpid());
+                printf("Listo para recibir la señal SIGUSR1.\n");
+                signal(SIGUSR1, signalHandler);
+		//tambien llamamos a signalHandler	
+		// al recivir la señal SIGUSR1
+}
+
+int main(int argc, char *argv[]){
+
+	int n = 0;
+	signal(SIGINT, signalCtrl);//Aqui llamo a la funcion signalCtr 
+    					//Cuando capta la señal SIGINT
+	while(n<1) {//Creamos un bucle para manter el proceso activo 
+		    //y se pausa para que no se ejecute constantemente 
+		start();
+        	pause();
+		sleep(2);
+
+   }
+      
+	return(EXIT_SUCCESS);
+    
+}
